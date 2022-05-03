e53f32e83b5005e389174291180269664363abb7
Yevgeny Carranza <lijiu001@gmail.com>
2022-05-03 18:59:46 -0400

    first

---
+++ src/main/java/collections/Collections.java
@@ -0,0 +1,38 @@
+package collections;
+
+import java.util.*;
+
+public class Collections {
+    public static void main(String[] args) {
+//        Queue fruit = new LinkedList();
+//        fruit.add("apple");
+//        fruit.add("lemon");
+//        fruit.add("banana");
+//        fruit.add("orange");
+//        fruit.add("lemon");
+//        System.out.println(fruit.remove());
+//        System.out.println(fruit.peek());
+//
+//        List unchangeableList = List.of("apple", "banana", "orange");
+
+//        setDemo();
+    }
+
+//    private static void setDemo() {
+//        Set<String> fruit = new HashSet();
+//        fruit.add("apple");
+//        fruit.add("lemon");
+//        fruit.add("banana");
+//        fruit.add("orange");
+//        fruit.add("lemon");
+//
+//        var i = fruit.iterator();
+//        while (i.hasNext()){
+//            System.out.println(i.next());
+//        }
+//        for(String item: fruit){
+//            System.out.println(item);
+//        }
+//        fruit.forEach(System.out::println);
+//    }
+}
